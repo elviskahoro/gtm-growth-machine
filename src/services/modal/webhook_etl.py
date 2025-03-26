8ab440128c97216a0974cffcd3b809308b92548e
elvis kahoro <elvis@chalk.ai>
2025-03-26 11:44:50 -0700

    lint: Lint

--- src/services/modal/webhook_etl.py
+++ src/services/modal/webhook_etl.py
@@ -126,7 +126,7 @@ def local(
 
     source_file_data: Iterator[SourceFileData] = get_source_file_data_from_input_folder(
         input_folder=input_folder,
-        base_model=WebhookModel, # trunk-ignore(pyright/reportArgumentType)
+        base_model=WebhookModel,  # trunk-ignore(pyright/reportArgumentType)
         extension=[
             ".json",
             ".jsonl",
